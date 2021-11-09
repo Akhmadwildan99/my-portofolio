e65958ca59230c8b8d241ccc0469ac78d0a1e222
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-11-09 20:38:34 +0700

    hanburger menu

--- components/Header/header.js
+++ components/Header/header.js
@@ -5,11 +5,16 @@ export default function Header() {
                 <div className=" col-span-3 w-50 ml-10 text-3xl  max-h-full italic font-serif font-bold text-black cursor-pointer">
                     Wildan
                 </div>
-                <ul className="col-span-4 flex justify-evenly px-3 items-center uppercase italic text-sm font-semibold">
-                    <li className="hover:scale-125 transition-all duration-75 ease-out cursor-pointer">Home</li>
-                    <li className="hover:scale-125 transition-all duration-75 ease-out cursor-pointer">About me</li>
-                    <li className="hover:scale-125 transition-all duration-75 ease-out cursor-pointer">Blog</li>
-                    <li className="hover:scale-125 transition-all duration-75 ease-out cursor-pointer">Contact</li>
+                <div className="md:hidden col-span-3 flex px-3 justify-end cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 hover:scale-125 duration-75 ease-out" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </div>
+                <ul className="hidden col-span-4 md:flex md:justify-evenly px-3 items-center uppercase italic text-sm font-semibold">
+                    <li className="md:hover:scale-125 md:transition-all md:duration-75 md:ease-out cursor-pointer">Home</li>
+                    <li className="md:hover:scale-125 md:transition-all md:duration-75 md:ease-out cursor-pointer">About me</li>
+                    <li className="md:hover:scale-125 md:transition-all md:duration-75 md:ease-out cursor-pointer">Blog</li>
+                    <li className="md:hover:scale-125 md:transition-all md:duration-75 md:ease-out cursor-pointer">Contact</li>
                 </ul>
             </div>
         </header>
